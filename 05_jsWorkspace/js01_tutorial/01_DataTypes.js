abbae796077ee2b2806d4828219a2bfdd85773f4
seulayoon <seulaayoon@gmail.com>
2021-09-27 16:12:21 +0900

    js 1일차

---
+++ 05_jsWorkspace/js01_tutorial/01_DataTypes.js
@@ -0,0 +1,22 @@
+/*
+const, let은 ES6버전부터 생긴 변수 선언 방식
+var는 기본적인 js의 변수 선언 방식
+*/
+const a = 5; //const는 상수 
+const b = 10;
+let myName = "seula"; //let은 바꿀 수 있음. 최초로 한 번 선언 하고 다음 부터는 바로 쓰기
+var c = 39; //오래된 자바스크립트 변수 선언 방식, let과 같은 방식으로 무책임^^
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+myName = "nico"; //let으로 선언했던 변수 값 다른 것 넣어줌.
+
+console.log("your new name is " + myName);
+
+const cool = true;
+const hot = false;
+const amIFat = null; // null = nothing, 값이 없음 그 자체
+let something;
+console.log(something); //undefined = variable은 존재하는데, 정의되지 않음. 즉, 컴퓨터 메모리는 할당 됐지만, 값은 없음.
