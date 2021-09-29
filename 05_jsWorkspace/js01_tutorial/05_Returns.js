e693bc947e53a63b92a131e126fa5add0636bbe8
seulayoon <seulaayoon@gmail.com>
2021-09-29 12:04:19 +0900

    js 3일차

---
+++ 05_jsWorkspace/js01_tutorial/05_Returns.js
@@ -0,0 +1,11 @@
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+    ageOfForeigner + 2; // function의 결과를 콘솔에 출력한다.
+    return "hello";
+}
+
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
+//console.log(ageOfForeigner); // function안에 있는 변수값을 꺼내서 출력할 수는 없음 
+
