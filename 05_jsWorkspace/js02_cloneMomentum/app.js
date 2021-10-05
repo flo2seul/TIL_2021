758abce6cbd8ffb18a7a0aea3132280c89225885
seulayoon <seulaayoon@gmail.com>
2021-10-05 10:50:41 +0900

    event2

--- 05_jsWorkspace/js02_cloneMomentum/app.js
+++ 05_jsWorkspace/js02_cloneMomentum/app.js
@@ -1,8 +1,17 @@
 const title = document.querySelector(".hello:first-child h1");
 
+console.dir(title);
 function handleTitleClick(){
     console.log("title was clicked");
 }
 
-title .addEventListener("click", handleTitleClick); //유저가 클릭할 때, js가 대신 눌러줌
+function handleMouseEnter(){
+    console.log("Mouse is here");
+}
 
+function handleMouseLeave(){
+    console.log("Mouse is gone");
+}
+title .addEventListener("click", handleTitleClick); //유저가 클릭할 때, js가 대신 눌러줌
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
