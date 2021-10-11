17b1f5aff6763778821a17e0fb2e5d8014e4f3eb
seulayoon <seulaayoon@gmail.com>
2021-10-11 12:14:37 +0900

    css in js

--- 05_jsWorkspace/js02_cloneMomentum/app.js
+++ 05_jsWorkspace/js02_cloneMomentum/app.js
@@ -1,34 +1,14 @@
-const h1 = document.querySelector(".hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(h1);
-function handleTitleClick(){
-    console.log("title was clicked");
+function handleTitleClick() {
+    //const clickedClass = "active";
+    // if (h1.classList.contains(clickedClass)) {
+    // h1.classList.remove(clickedClass);    
+    //} else {
+        // h1.class.List.add(clickedClass);
+    //}
+    h1.classList.toggle("active")
 }
 
-function handleMouseEnter(){
-    console.log("Mouse is here");
-}
 
-function handleMouseLeave(){
-    console.log("Mouse is gone");
-}
-function handleWindowResize(){
-     document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy(){
-    alert("copier!");
-}
-function handleWindowOffline(){
-    alert("SOS no wifi");
-}
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
-h1.onclick = handleTitleClick; //유저가 클릭할 때, js가 대신 눌러줌
-h1.addEventListener("mouseenter",handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
