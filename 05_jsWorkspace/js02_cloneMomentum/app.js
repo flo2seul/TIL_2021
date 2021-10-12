e2a4927ecf283e6ef701bfce1cac328fe71e059c
seulayoon <seulaayoon@gmail.com>
2021-10-12 10:38:39 +0900

    Events part Two

--- 05_jsWorkspace/js02_cloneMomentum/app.js
+++ 05_jsWorkspace/js02_cloneMomentum/app.js
@@ -1,9 +1,14 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form button ");
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    console.log(loginInput.value);
     }
-
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
