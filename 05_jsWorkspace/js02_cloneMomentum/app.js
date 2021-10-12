315e107e884afa2debb5a36091684d287f3abf9c
seulayoon <seulaayoon@gmail.com>
2021-10-12 10:14:27 +0900

    Form Submission

--- 05_jsWorkspace/js02_cloneMomentum/app.js
+++ 05_jsWorkspace/js02_cloneMomentum/app.js
@@ -2,8 +2,10 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button ");
 
 function onLoginBtnClick() {
-    console.dir("hello", loginInput.value);
+    const username = loginInput.value;
+    console.log(username);
+    }
 
-}
+
 
 loginButton.addEventListener("click", onLoginBtnClick);
