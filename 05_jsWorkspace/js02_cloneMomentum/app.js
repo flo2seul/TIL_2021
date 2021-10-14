e99914e34cd3180899d760761ffd466dfb5c1539
seulayoon <seulaayoon@gmail.com>
2021-10-14 12:18:42 +0900

    Getting Username

--- 05_jsWorkspace/js02_cloneMomentum/app.js
+++ 05_jsWorkspace/js02_cloneMomentum/app.js
@@ -1,14 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form button ");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    console.log(username);
+    greeting.innerText = `Hello ${username}`; //변수랑 string합쳐서 내보내기 `${}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
     }
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
-}
+
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click",handleLinkClick);
